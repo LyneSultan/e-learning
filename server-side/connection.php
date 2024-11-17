2e8e84eeda44a632420e9fa667d7635796f0b62e
LyneSultan <lynesultane@gmail.com>
2024-11-17 22:22:45 +0200

    add - headers

--- server-side/connection.php
+++ server-side/connection.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $connection= new mysqli("localhost","root","","e-learning_db");
 
 if ($connection->connect_error) {

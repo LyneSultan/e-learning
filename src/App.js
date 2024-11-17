d11f235c6fb834c083bba1b6b0c3e19705e36fae
LyneSultan <lynesultane@gmail.com>
2024-11-17 22:23:34 +0200

    add - login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate(); // Usage
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    const data = new FormData();
+    data.append("username", username);
+    data.append("password", password);
+
+    fetch("http://localhost/e-learning/server-side/login.php", {
+      method: "POST",
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.user.user_type);
+        if (data.user.user_type === "admin") {
+          console.log("data.user.user_type");
+          navigate("/admin");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          placeholder="enter your name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={login}>Log in</button>
     </div>
   );
 }

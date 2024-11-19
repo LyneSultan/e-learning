7cc628f76b0e43ea2698f304cf2268750c00ec58
LyneSultan <lynesultane@gmail.com>
2024-11-19 09:34:27 +0200

    create - api to get students,courses and instructors

---
+++ server-side/insert_instructor.php
@@ -0,0 +1,14 @@
+<?php
+include 'connection.php';
+
+$username=$_POST['username'];
+
+ $query = $connection->prepare("INSERT INTO users (username, user_type) VALUES (?,  'instructor')");
+ $query->bind_param("s", $username);
+
+
+ if ($query->execute()) {
+     echo json_encode(["success" => "Instructor created successfully."]);
+ } else {
+     echo json_encode(["error" => "Failed to create instructor."]);
+ }
---
+++ server-side/view_all.php
@@ -0,0 +1,43 @@
+<?php
+include 'connection.php';
+$students=[];
+$instructors=[];
+$courses=[];
+
+$query_students = $connection->prepare("SELECT * FROM users WHERE user_type = 'student'");
+$query_students->execute();
+
+$result_students=$query_students->get_result();
+if($result_students->num_rows>0){
+  while ($row = $result_students->fetch_assoc()) {
+    $students[] = $row;
+  }
+}
+
+$query_instructors = $connection->prepare("SELECT * FROM users WHERE user_type = 'instructor'");
+$query_instructors->execute();
+
+$result_instructors=$query_instructors->get_result();
+if($result_instructors->num_rows>0){
+  while ($row = $result_instructors->fetch_assoc()) {
+    $instructors[] = $row;
+  }
+}
+
+
+$query_courses = $connection->prepare("SELECT * FROM courses");
+$query_courses->execute();
+
+$result_courses=$query_courses->get_result();
+if($result_courses->num_rows>0){
+  while ($row = $result_courses->fetch_assoc()) {
+    $courses[] = $row;
+  }
+}
+
+echo json_encode([
+  "students"=>$students,
+  "instructors"=>$instructors,
+  "courses"=>$courses
+]);
+

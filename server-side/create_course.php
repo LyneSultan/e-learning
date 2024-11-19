9cc9cca4b45e28174205f32a67e91e5093495174
LyneSultan <lynesultane@gmail.com>
2024-11-19 09:28:45 +0200

    create - course api

---
+++ server-side/create_course.php
@@ -0,0 +1,17 @@
+<?php
+include "connection.php";
+
+ $title = $_POST["title"] ?? NULL;
+
+ if ($title) {
+     $query = $connection->prepare("INSERT INTO courses (title) VALUES (?)");
+     $query->bind_param("s", $title);
+
+     if ($query->execute()) {
+         echo json_encode(["message" => "Course created successfully"]);
+     } else {
+         echo json_encode(["message" => "Failed to create course"]);
+     }
+ } else {
+     echo json_encode(["message" => "Required data missing"]);
+ }

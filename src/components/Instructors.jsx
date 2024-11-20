e613d4a8a185cf0ed3a65b1b0360c8d0f582c8f1
LyneSultan <lynesultane@gmail.com>
2024-11-21 01:19:46 +0200

    edit - the instructors component

--- src/components/Instructors.jsx
+++ src/components/Instructors.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { handleBanUnban } from "./../utils/handleBanButton";
+import DataTable from "../utils/table";
+import './../styles/base.css';
 
-function Instructors() {
-  const location = useLocation();
-
-  const { instructors, courses } = location.state;
-  console.log(instructors);
+function Instructors({ instructors, courses, setInstructors }) {
   console.log(courses);
+  console.log(instructors);
 
   const [instructor, setInstructor] = useState("");
   const [selectedInstructor, setSelectedInstructor] = useState("");
@@ -22,96 +19,181 @@ function Instructors() {
         body: data,
       }
     ).then(response => response.json())
-      .then(data => { console.log(data); })
+      .then(data => {
+        console.log(data);
+        setInstructors(() => [
+          ...instructors,
+          { user_id: data.instructor_id, username: data.username, user_type: "instructor" },
+        ]);
+
+      })
       .catch(error => {
         console.error("Error:", error);
       })
   }
   return (
-    <>
+    <div className="flex flex-column ">
       <h1>Create instructor record and View all instructors</h1>
 
-      <div>
-        <input
-          type="text"
-          placeholder="enter the instructor name"
-          onChange={(e) => setInstructor(e.target.value)}
-        />
-        <button onClick={() => createInstructor(instructor)}>add this instructor</button>
+      <div className="flex space-around">
 
-      </div>
+        <div className="flex flex-column">
+          <input
+            type="text"
+            placeholder="enter the instructor name"
+            onChange={(e) => setInstructor(e.target.value)}
+          />
+          <button onClick={() => createInstructor(instructor)}>add this instructor</button>
 
-      <div>
-        <label>Select Instructor:</label>
-        <select value={selectedInstructor}
-          onChange={(e) => {
-            setSelectedInstructor(e.target.value); // it is not getting the latest change
-            console.log(selectedInstructor);
-          }}>
-          {instructors.map((instructor) => (
-            <option value={instructor.user_id}>
-              {instructor.username}
-            </option>
-          ))}
-        </select>
-        <label>select the course</label>
-        <select value={selectedCourse}
-          onChange={(e) => {
-            setSelectedCourse(e.target.value);
-            console.log(selectedCourse);
-          }}>
-          {courses.map((course) => (
-            <option value={course.course_id}>
-              {course.title}
-            </option>
-          ))}
-        </select>
-        <button onClick={() => {
-          const data = new FormData();
-          data.append("course_id", selectedCourse);
-          data.append("instructor_id", selectedInstructor);
-          fetch("http://localhost/e-learning/server-side/assign_instructor.php", {
-            method: "POST",
-            body: data
-          }).then(response => response.json())
-            .then(data => {
-              console.log(selectedCourse);
+        </div>
+
+        <div className="flex flex-column">
+          <label>Select Instructor:</label>
+          <select value={selectedInstructor}
+            onChange={(e) => {
+              setSelectedInstructor(e.target.value); // it is not getting the latest change
               console.log(selectedInstructor);
-              console.log(data);
+            }}>
+            {instructors.map((instructor) => (
+              <option value={instructor.user_id}>
+                {instructor.username}
+              </option>
+            ))}
+          </select>
+          <label>select the course</label>
 
-            })
-            .catch(error => {
-              console.error('Error:', error);
-            });
+          <select value={selectedCourse}
+            onChange={(e) => {
+              setSelectedCourse(e.target.value);
+              console.log(selectedCourse);
+            }}>
+            {courses.map((course) => (
+              <option value={course.course_id}>
+                {course.title}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => {
+            const data = new FormData();
+            data.append("course_id", selectedCourse);
+            data.append("instructor_id", selectedInstructor);
 
-        }
-        }>assign</button>
+            fetch("http://localhost/e-learning/server-side/assign_instructor.php", {
+              method: "POST",
+              body: data
+            }).then(response => response.json())
+              .then(data => {
+                console.log(selectedCourse);
+                console.log(selectedInstructor);
+                console.log(data);
+
+              })
+              .catch(error => {
+                console.error('Error:', error);
+              });
+          }}>assign</button>
+        </div>
+        <DataTable data={instructors} />
       </div>
-
-
-
-      <table>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {instructors.map((instructor, index) => (
-            <tr key={index}>
-              <td>{instructor.username}</td>
-              <td>
-                <button onClick={() => handleBanUnban(instructor.user_id, 1)}>Ban</button>
-                <button onClick={() => handleBanUnban(instructor.user_id, 0)}>Unban</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-
-    </>
+    </div>
   )
 }
 export default Instructors;
+// import React, { useState } from "react";
+// import DataTable from "../utils/table";
+// import './../styles/base.css';
+
+// function Instructors({ instructors, courses, setInstructors }) {
+//   const [instructor, setInstructor] = useState("");
+//   const [selectedInstructor, setSelectedInstructor] = useState("");
+//   const [selectedCourse, setSelectedCourse] = useState("");
+
+//   const createInstructor = (instructor_name) => {
+//     const data = new FormData()
+//     data.append("username", instructor_name);
+//     fetch("http://localhost/e-learning/server-side/create_instructor.php",
+//       {
+//         method: "POST",
+//         body: data,
+//       }
+//     ).then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+
+//       })
+//       .catch(error => {
+//         console.error("Error:", error);
+//       })
+//   }
+//   return (
+//     <div className="flex flex-column ">
+//       <h1>Create instructor record and View all instructors</h1>
+
+//       <div class="flex space-around">
+
+//         <div class="flex flex-column">
+//           <input
+//             type="text"
+//             placeholder="enter the instructor name"
+//             onChange={(e) => setInstructor(e.target.value)}
+//           />
+//           <button onClick={() => createInstructor(instructor)}>add this instructor</button>
+
+//         </div>
+
+//         <div className="flex flex-column">
+//           <label>Select Instructor:</label>
+//           <select value={selectedInstructor}
+//             onChange={(e) => {
+//               setSelectedInstructor(e.target.value); // it is not getting the latest change
+//               console.log(selectedInstructor);
+//             }}>
+//             {instructors.map((instructor) => (
+//               <option value={instructor.user_id}>
+//                 {instructor.username}
+//               </option>
+//             ))}
+//           </select>
+//           <label>select the course</label>
+
+//           <select value={selectedCourse}
+//             onChange={(e) => {
+//               setSelectedCourse(e.target.value);
+//               console.log(selectedCourse);
+//             }}>
+//             {courses.map((course) => (
+//               <option value={course.course_id}>
+//                 {course.title}
+//               </option>
+//             ))}
+//           </select>
+//           <button onClick={() => {
+//             const data = new FormData();
+//             data.append("course_id", selectedCourse);
+//             data.append("instructor_id", selectedInstructor);
+
+//             fetch("http://localhost/e-learning/server-side/assign_instructor.php", {
+//               method: "POST",
+//               body: data
+//             }).then(response => response.json())
+//               .then(data => {
+//                 console.log(selectedCourse);
+//                 console.log(selectedInstructor);
+//                 console.log(data);
+
+//               })
+//               .catch(error => {
+//                 console.error('Error:', error);
+//               });
+//           }}>assign</button>
+//         </div>
+
+
+//         <DataTable data={instructors} />
+
+
+//       </div>
+//     </div>
+//   )
+// }
+// export default Instructors;

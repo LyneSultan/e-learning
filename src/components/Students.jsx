a3a8df18099e5b43b809c4bc0a95723e929d27b2
LyneSultan <lynesultane@gmail.com>
2024-11-21 01:18:19 +0200

    use - table component

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,38 +1,16 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
-import { handleBanUnban } from "./../utils/handleBanButton";
-
-const Students = () => {
-  const location = useLocation();
-  const { students } = location.state || { students: [] };
+import DataTable from "../utils/table";
+import './../styles/base.css';
+const Students = ({ students }) => {
 
   console.log(students);
 
   return (
-    <>
-      <h1>Student</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={index}>
-              <td>{student.username}</td>
-              <td>
-                <button onClick={() => handleBanUnban(student.user_id, 1)} >Ban</button>
-                <button onClick={() => handleBanUnban(student.user_id, 0)}>Unban</button>
-
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div >
+      <DataTable data={students} />
+    </div>
   );
 };
 
 export default Students;
+

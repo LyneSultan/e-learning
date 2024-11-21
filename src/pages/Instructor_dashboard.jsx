716dce11af10474a3e13802e864c697af01a9369
LyneSultan <lynesultane@gmail.com>
2024-11-21 17:03:14 +0200

    edit - instructor dashboard component

--- src/pages/Instructor_dashboard.jsx
+++ src/pages/Instructor_dashboard.jsx
@@ -1,5 +1,51 @@
-import Course_card from "./../components/Course_card";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './../styles/base.css';
+
 const Instructor_dashboard = () => {
-  return (<Course_card title={"test"} />)
+  const navigate = useNavigate();
+  const [instructorCourses, setInstructorCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost/e-learning/server-side/instructor_courses.php", {
+      method: "POST",
+      headers: { "Authorization": localStorage.token }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setInstructorCourses(data.instructor_courses);
+      })
+      .catch((e) => console.log(e))
+  }, [])
+
+  const [students, setStudents] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost/e-learning/server-side/view_all.php")
+      .then((response) => response.json())
+      .then((data) => {
+        setStudents(data.students);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+
+  return (<>
+    <h1>your courses</h1>
+    {console.log(students)}
+
+    <div className="flex space-around flec-wrap align-center">
+      {
+        instructorCourses.map((instructorCourse) => (
+          <div className="card"
+            key={instructorCourse.id}
+            onClick={() => navigate(`/instructor/course/${instructorCourse.course_id}`)}>
+            {instructorCourse.title}
+          </div>
+        ))
+      }</div>
+  </>)
 }
 export default Instructor_dashboard;
